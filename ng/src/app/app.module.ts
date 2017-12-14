869b4ebf639e21712acff33e21295ecefa5e073d
SAPIENT\vsha95 <vsharma109@sapient.com>
2017-12-14 14:29:37 +0530

    new component generated

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
